d40cd817f8c72ff1b4b14e17ede27c6cc7f22601
Paul Harter <pl.hrtr@gmail.com>
2026-01-05 19:00:26 +0100

    fixed config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
-import fs from "fs";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
-    },
-    host: "localhost", // optional
-    port: 5173, // default
-  },
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  base: "/notes/",
 });
